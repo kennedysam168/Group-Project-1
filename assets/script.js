cc377d112ba728814024a4449c9985090301f38d
Mallory Schneider <mallorysschneider@gmail.com>
2021-10-13 19:39:36 -0400

    argh I hope i dont braek it

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,14 @@
 console.log("I work!");
 
+
+const sBtn = document.getElementById("searchBtn");
+
+
+function search() {
+    // console.log("clicked")
+    document.getElementById("search-result").style.transition = "all 2s ease-in-out";
+    document.getElementById("search-result").style.opacity = 1;
+
+}
+
+sBtn.addEventListener('click', search);
