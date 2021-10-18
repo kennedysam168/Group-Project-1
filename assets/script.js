4ad08aaa8452c4e69bb68b2849ef3584cc994053
Mallory Schneider <mallorysschneider@gmail.com>
2021-10-18 19:00:56 -0400

    I added some more lgbtq friendly help sites

--- assets/script.js
+++ assets/script.js
@@ -22,16 +22,16 @@ function setupMap(center) {
         style: 'mapbox://styles/mapbox/streets-v11',
         center: center,
         zoom: 13
-      });
+    });
 
-      const nav = new mapboxgl.NavigationControl();
-      map.addControl(nav, 'bottom-left');
+    const nav = new mapboxgl.NavigationControl();
+    map.addControl(nav, 'bottom-left');
 
-      var directions = new MapboxDirections({
+    var directions = new MapboxDirections({
         accessToken: 'pk.eyJ1IjoiY2hyaXN0aWFuYW50aSIsImEiOiJja3VxNmhlc2Eyd2lrMnVvZjAyMDRtemVqIn0.Ml5z5JIn9PNbYQqRnQOcFQ',
-      });
-      
-      map.addControl(directions, 'top-left');
+    });
+
+    map.addControl(directions, 'top-left');
 }
 
 // var map = new mapboxgl.Map({
@@ -57,9 +57,9 @@ var searchResults = document.getElementById("search-result")
 searchResults.style.display = "none";
 sBtn.addEventListener("click", search());
 
-function search(){
-    searchResults.style.display = "block";
+function search() {
+    searchResults.style.display = "all";
     console.log("search works")
 }
-    
+
 
